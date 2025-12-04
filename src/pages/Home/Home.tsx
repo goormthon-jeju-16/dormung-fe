dc377e96888f7f50a85a03228835a9f198974ec3
chahyunlee <chahyun023@gmail.com>
2025-12-04 15:09:12 +0900

    feat: 온보딩 페이지 구현 및 홈 화면 구현

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,26 @@
-import { Text } from '@vapor-ui/core'
+import { Box, Button, VStack } from '@vapor-ui/core'
 
 const HomePage = () => {
-  return <Text>Hello World!</Text>
+  return (
+    <VStack
+      alignItems="center"
+      justifyContent="center"
+      gap="$400"
+      padding="$500"
+      style={{
+        minHeight: '100vh',
+        width: '100%',
+      }}
+    >
+      <Box render={<h3 />}>도르멍</Box>
+      <Box render={<p />}>
+        우리 서비스는 어쩌구 입니다. <br /> 어떻게 서비스를 시작해보아요
+      </Box>
+      <Button colorPalette="primary" size="lg" variant="fill" width="100%">
+        프로필 입력하러 가기
+      </Button>
+    </VStack>
+  )
 }
 
 export default HomePage
--- src/pages/Onboarding/OnboardingProfile.tsx
+++ src/pages/Onboarding/OnboardingProfile.tsx
@@ -1,16 +1,8 @@
 import { useState } from 'react'
-import {
-  Box,
-  Field,
-  Text,
-  TextInput,
-  VStack,
-  Button,
-  Select,
-} from '@vapor-ui/core'
+import { Box, Field, TextInput, VStack, Button, Select } from '@vapor-ui/core'
 import { SelectButtonGroup } from '@/pages/Onboarding/components/SelectButtonGroup'
 
-const OnboardingPage = () => {
+const OnboardingProfilePage = () => {
   const [selectedPeriod, setSelectedPeriod] = useState<string | null>(null)
   const [selectedAge, setSelectedAge] = useState<string | null>(null)
   const [selectedGender, setSelectedGender] = useState<string | null>(null)
@@ -31,27 +23,41 @@ const OnboardingPage = () => {
     { id: 'male', label: '남성' },
   ]
 
+  const regions = [
+    { value: 'yeonno-nohyeong', label: '연동/노형' },
+    { value: 'ara', label: '아라동' },
+    { value: 'downtown', label: '원도심/이도/일도/삼도/용담' },
+    { value: 'aewol', label: '애월' },
+    { value: 'hallim-hyeopjae', label: '한림/협재' },
+    { value: 'jocheon-hamdeok', label: '조천/함덕' },
+    { value: 'jungmun-saekdal', label: '중문/색달' },
+    { value: 'seogwipo-downtown', label: '서귀포시내/서귀동/동홍' },
+    { value: 'namwon-wimi', label: '남원/위미' },
+    { value: 'seongsan-pyoseon', label: '성산/표선' },
+  ]
+
   return (
     <div
       style={{
         display: 'flex',
         flexDirection: 'column',
         gap: '20px',
-        paddingTop: 'var(--vapor-size-space-200)',
+        paddingTop: 'var(--vapor-size-space-500)',
         paddingBottom: 'var(--vapor-size-space-250)',
-        paddingLeft: 'var(--vapor-size-space-500)',
-        paddingRight: 'var(--vapor-size-space-500)',
+        paddingLeft: 'var(--vapor-size-space-400)',
+        paddingRight: 'var(--vapor-size-space-400)',
       }}
     >
-      <Text render={<h3 />} foreground="normal-200">
+      <Box
+        render={<h3 />}
+        className="text-center"
+        marginBottom="var(--vapor-size-space-200)"
+      >
         회원가입
-      </Text>
-
+      </Box>
       <Field.Root name="nickname" className="w-full">
         <Box render={<Field.Label />} flexDirection="column" className="gap-2">
-          <Text render={<h5 />} foreground="secondary-100">
-            닉네임을 입력해주세요.
-          </Text>
+          <Box render={<h3 />}>닉네임을 입력해주세요.</Box>
           <Box
             borderColor="var(--color-border-normal)"
             borderRadius="var(--vapor-size-radius-100)"
@@ -60,21 +66,18 @@ const OnboardingPage = () => {
           </Box>
         </Box>
       </Field.Root>
-
       <SelectButtonGroup
         label="제주에 오신지 얼마나 되셨나요?"
         options={periods}
         selectedValue={selectedPeriod}
         onSelect={setSelectedPeriod}
       />
-
       <SelectButtonGroup
         label="연령대를 골라주세요."
         options={ages}
         selectedValue={selectedAge}
         onSelect={setSelectedAge}
       />
-
       <SelectButtonGroup
         label="성별을 골라주세요."
         options={genders}
@@ -83,19 +86,24 @@ const OnboardingPage = () => {
       />
 
       <VStack>
-        <Text>거주 지역은 어디인가요?</Text>
-        <Select.Root placeholder="지역 선택">
-          <Select.Trigger width="300px" />
+        <Box marginBottom="var(--vapor-size-space-100)" render={<h4 />}>
+          거주 지역은 어디인가요?
+        </Box>
+        <Select.Root size="lg" placeholder="지역 선택">
+          <Select.Trigger />
           <Select.Positioner>
             <Select.Popup>
               <Select.Group>
-                <Select.Item value="sans-serif">Sans-serif</Select.Item>
+                {regions.map(region => (
+                  <Select.Item key={region.value} value={region.value}>
+                    {region.label}
+                  </Select.Item>
+                ))}
               </Select.Group>
             </Select.Popup>
           </Select.Positioner>
         </Select.Root>
       </VStack>
-
       <Button colorPalette="primary" size="lg" variant="fill" width="100%">
         다음
       </Button>
@@ -103,4 +111,4 @@ const OnboardingPage = () => {
   )
 }
 
-export default OnboardingPage
+export default OnboardingProfilePage
